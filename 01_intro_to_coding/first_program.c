27ed377b04f15b78442d8599774ddad1fc760615
Hwalteo Kim <hkim0232@floridapoly.edu>
2025-08-22 16:13:15 +0000

    escape sequence

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,19 @@
+/*
+Multi-line comment.
+08/20/2025
+Hwalteo Kim
+The first program in C.
+*/
+// One Line commet
+// Preprocessor directive
+// Studio.h - standard input output
+// .h - header file
+#include <stdio.h>
+// Create the main function
+int main(void) {
+    printf("hello\nWorld\n");  //\n(new line) - escape sequence
+    printf("I\nam\nready\nto\ncode!\n");
+    printf("A\tb\tc\n");
+    return 0;
+}
+

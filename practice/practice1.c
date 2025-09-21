1be4a6dc86b929055da8cd8c45d437384c6b5bde
Hwalteo Kim <hkim0232@floridapoly.edu>
2025-09-21 21:05:02 +0000

    one two

--- practice/practice1.c
+++ practice/practice1.c
@@ -2,25 +2,20 @@
 
 void one_three(void);
 void two(void);
-void three(void);
 
 int main(void) {
-    printf("starting now:\n");
-    one_three();      // one_three()를 호출하면 → one, two, three까지 출력
-    printf("done!\n");
+    puts("starting now:");
+    one_three();
+    puts("done!");
     return 0;
 }
 
-void one_three(void) {
-    printf("one\n");  // one_three가 먼저 "one" 출력
-    two();            // 그 다음 "two"
-    three();          // 그 다음 "three"
-}
-
 void two(void) {
-    printf("two\n");
+    puts("two");
 }
 
-void three(void) {
-    printf("three\n");
+void one_three(void) {
+    puts("one");
+    two();
+    puts("three");
 }
